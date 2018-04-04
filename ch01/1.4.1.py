17c951b6f9d55d8563772c34dcce374e3ff2b1f5
yunshu <yunshu_luo@163.com>
2018-04-04 22:49:23 +0800

    # 兼容python3 添加注释

--- ch01/1.4.1.py
+++ ch01/1.4.1.py
@@ -1,40 +1,67 @@
-#coding:utf-8
-'''
-第一种方式：使用os模块中的fork方式实现多进程
+# coding:utf-8
 
+# 第一种方式：使用os模块中的fork方式实现多进程
+'''
 import os
 if __name__ == '__main__':
-    print 'current Process (%s) start ...'%(os.getpid())
+    print('current Process (%s) start ...' % (os.getpid()))
     pid = os.fork()
     if pid < 0:
-        print 'error in fork'
+        print('error in fork')
     elif pid == 0:
-        print 'I am child process(%s) and my parent process is (%s)',(os.getpid(),os.getppid())
+        print('I am child process(%s) and my parent process is (%s)' % (os.getpid(),os.getppid()))
     else:
-        print 'I(%s) created a chlid process (%s).',(os.getpid(),pid)
+        print('I(%s) created a chlid process (%s).' % (os.getpid(),pid))
 '''
 '''
-第二种方法：使用multiprocessing模块创建多进程
+输出结果：
+current Process (19821) start ...
+I(19821) created a chlid process (19822).
+I am child process(19822) and my parent process is (19821)
+'''
+''''
+# 第二种方法：使用multiprocessing模块创建多进程
 import os
 from multiprocessing import Process
+
 # 子进程要执行的代码
+
+
 def run_proc(name):
-    print 'Child process %s (%s) Running...' % (name, os.getpid())
+    print('Child process %s (%s) Running...' % (name, os.getpid()))
+
+
 if __name__ == '__main__':
-    print 'Parent process %s.' % os.getpid()
+    print('Parent process %s.' % os.getpid())
     p_list=[]
     for i in range(5):
+        # 使用Process创建进程时，需要传入一个执行函数和参数的参数
         p = Process(target=run_proc, args=(str(i),))
         p_list.append(p)
-        print 'Process will start.'
+        print('Process will start.')
+        # 使用start方法启动进程
         p_list[i].start()
     for p in p_list:
+        # 使用join方法实现进程的同步
         p.join()
-    print 'Process end.'
+    print('Process end.')
+'''
+'''
+运行结果：
+Parent process 20379.
+Process will start.
+Process will start.
+Process will start.
+Child process 0 (20380) Running...
+Process will start.
+Child process 1 (20381) Running...
+Process will start.
+Child process 2 (20382) Running...
+Child process 3 (20383) Running...
+Child process 4 (20384) Running...
+'''
 
-'''
-'''
-multiprocessing模块提供了一个Pool类来代表进程池对象
+# multiprocessing模块提供了一个Pool类来代表进程池对象
 
 from multiprocessing import Pool
 import os, time, random
@@ -54,7 +81,6 @@ if __name__=='__main__':
     p.join()
     print 'All subprocesses done.'
 
-'''
 '''
 Queue进程间通信
 
